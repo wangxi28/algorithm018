e419e992cba4a3843f2b5a67cf685130af673718
wxj <xinjuan28@163.com>
2020-10-25 22:23:01 +0800

    641. 设计循环双端队列

---
+++ week01/MyCircularDeque.java
@@ -0,0 +1,138 @@
+//设计实现双端队列。 
+//你的实现需要支持以下操作： 
+//
+// 
+// MyCircularDeque(k)：构造函数,双端队列的大小为k。 
+// insertFront()：将一个元素添加到双端队列头部。 如果操作成功返回 true。 
+// insertLast()：将一个元素添加到双端队列尾部。如果操作成功返回 true。 
+// deleteFront()：从双端队列头部删除一个元素。 如果操作成功返回 true。 
+// deleteLast()：从双端队列尾部删除一个元素。如果操作成功返回 true。 
+// getFront()：从双端队列头部获得一个元素。如果双端队列为空，返回 -1。 
+// getRear()：获得双端队列的最后一个元素。 如果双端队列为空，返回 -1。 
+// isEmpty()：检查双端队列是否为空。 
+// isFull()：检查双端队列是否满了。 
+// 
+//
+// 示例： 
+//
+// MyCircularDeque circularDeque = new MycircularDeque(3); // 设置容量大小为3
+//circularDeque.insertLast(1);			        // 返回 true
+//circularDeque.insertLast(2);			        // 返回 true
+//circularDeque.insertFront(3);			        // 返回 true
+//circularDeque.insertFront(4);			        // 已经满了，返回 false
+//circularDeque.getRear();  				// 返回 2
+//circularDeque.isFull();				        // 返回 true
+//circularDeque.deleteLast();			        // 返回 true
+//circularDeque.insertFront(4);			        // 返回 true
+//circularDeque.getFront();				// 返回 4
+//  
+//
+// 
+//
+// 提示： 
+//
+// 
+// 所有值的范围为 [1, 1000] 
+// 操作次数的范围为 [1, 1000] 
+// 请不要使用内置的双端队列库。 
+// 
+// Related Topics 设计 队列 
+// 👍 60 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class MyCircularDeque {
+    private int[] elements;
+    private int size;
+
+    /** Initialize your data structure here. Set the size of the deque to be k. */
+    public MyCircularDeque(int k) {
+        elements = new int[k];
+    }
+    
+    /** Adds an item at the front of Deque. Return true if the operation is successful. */
+    public boolean insertFront(int value) {
+        if (isFull()) {
+            return false;
+        }
+        for ( int i=size-1; i>=0; i-- ) {
+            elements[i+1] = elements[i];
+        }
+        elements[0] = value;
+        size ++;
+        return true;
+    }
+    
+    /** Adds an item at the rear of Deque. Return true if the operation is successful. */
+    public boolean insertLast(int value) {
+        if(isFull()){
+            return false;
+        }
+        elements[size] = value;
+        size++;
+        return true;
+    }
+    
+    /** Deletes an item from the front of Deque. Return true if the operation is successful. */
+    public boolean deleteFront() {
+        if(isEmpty()){
+            return false;
+        }
+        elements[0] = 0;
+        for(int i=0; i<size-1; i++){
+            elements[i]=elements[i+1];
+        }
+        size--;
+        return true;
+
+    }
+    
+    /** Deletes an item from the rear of Deque. Return true if the operation is successful. */
+    public boolean deleteLast() {
+        if(isEmpty()){
+            return false;
+        }
+        elements[size-1] = 0;
+        size--;
+        return true;
+
+    }
+    
+    /** Get the front item from the deque. */
+    public int getFront() {
+        return size == 0 ? -1 : elements[0];
+
+    }
+    
+    /** Get the last item from the deque. */
+    public int getRear() {
+        return size == 0 ? -1 : elements[size-1];
+
+    }
+    
+    /** Checks whether the circular deque is empty or not. */
+    public boolean isEmpty() {
+        return size == 0;
+
+    }
+    
+    /** Checks whether the circular deque is full or not. */
+    public boolean isFull() {
+        return size == elements.length;
+
+    }
+}
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque obj = new MyCircularDeque(k);
+ * boolean param_1 = obj.insertFront(value);
+ * boolean param_2 = obj.insertLast(value);
+ * boolean param_3 = obj.deleteFront();
+ * boolean param_4 = obj.deleteLast();
+ * int param_5 = obj.getFront();
+ * int param_6 = obj.getRear();
+ * boolean param_7 = obj.isEmpty();
+ * boolean param_8 = obj.isFull();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
